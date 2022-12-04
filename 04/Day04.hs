2e36b909883afb478bd4282c864175798ad83362
waynee95 <larsericmarquardt@mail.de>
2022-12-04 14:19:31 +0100

    Day 04

---
+++ 04/Day04.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day04 where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+
+import Text.Printf
+
+parseInput :: T.Text -> [((Int, Int), (Int, Int))]
+parseInput = map rangeToNums . map splitRanges . map (T.splitOn ",") . T.lines
+  where
+    splitRange r = T.splitOn "-" r
+    splitRanges [x, y] = (splitRange x, splitRange y)
+
+    toNum s = let (Right (n, _)) = T.decimal s in n
+    rangeToNums ([x1, x2], [y1, y2]) = ((toNum x1, toNum x2), (toNum y1, toNum y2))
+
+overlapFully :: (Int, Int) -> (Int, Int) -> Bool
+overlapFully (x1, x2) (y1, y2) = (x1 >= y1 && x2 <= y2) || (y1 >= x1 && y2 <= x2)
+
+overlapAny :: (Int, Int) -> (Int, Int) -> Bool
+overlapAny (x1, x2) (y1, y2) =
+    (x1 <= y1 && y1 <= x2)
+        || (x1 <= y2 && y2 <= x2)
+        || (y1 <= x1 && x1 <= y2)
+        || (y1 <= x2 && x2 <= y2)
+
+-- (uncurry overlapAny) :: ((Int, Int), (Int, Int)) -> Bool
+solve f = length . filter ((==) True) . map (uncurry f)
+
+main :: IO ()
+main = do
+    pairList <- parseInput <$> T.readFile "input.txt"
+    printf "Part 1: %d\n" $ solve overlapFully pairList
+    printf "Part 2: %d\n" $ solve overlapAny pairList
